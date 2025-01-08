44e26a2e54b4eda2630446781f0af836eafca017
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-09 01:06:04 +0600

    successfully conntect the server to database done on 01/09/2025 at 01:06 am

--- index.js
+++ index.js
@@ -5,13 +5,40 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-app.use(cors());
-app.use(express.json());
-
 const app = express();
 const port = process.env.PORT || 2000;
 const username = process.env.DB_USER;
 const password = process.env.DB_PASS;
+const { MongoClient, ServerApiVersion } = mongodb;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.6zld8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('server running on 2000');
