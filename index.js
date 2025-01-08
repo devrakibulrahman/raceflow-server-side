838f46a61c0493297a1b9bc4004be8a4201c19e6
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-09 00:58:15 +0600

    create server and run the server done on 01/09/2025 at 12:58 am

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongodb from 'mongodb';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 2000;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASS;
+
+app.get('/', (req, res) => {
+    res.send('server running on 2000');
+});
+
+app.listen(port, (req, res) => {
+    console.log(`http://localhost:${port}`);
+});
