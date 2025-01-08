c7a886e5a7d052b9f3ca745fdd79c6894430ed77
devrakibulrahman <devrakibulrahman@gmail.com>
2025-01-09 01:12:51 +0600

    create marathon collection in the database done on 01/09/2025 at 01:12 am

--- index.js
+++ index.js
@@ -30,11 +30,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    //? database collections ----------------------------------------------------->
+    const marathon_collection = client.db('raceflow').collection('marathons');
+
     //? marathons apis ----------------------------------------------------------->
-    app.post('/marathon', (req, res) => {
-        const marathon_data = req.body;
-        console.log(marathon_data);
-    });
+    // app.post('/marathon', (req, res) => {
+    //     const marathon_data = req.body;
+    //     console.log(marathon_data);
+    // });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
